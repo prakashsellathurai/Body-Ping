7de478bc1e3345cf9d1d612deb72ac496c3216b9
prakash <prakashsellathurai@gmail.com>
2020-05-20 13:32:04 +0530

    removed the Plus icon

--- lib/screens/dashboard/bottom_navigation_view/bottom_bar_view.dart
+++ lib/screens/dashboard/bottom_navigation_view/bottom_bar_view.dart
@@ -1,9 +1,10 @@
 import 'dart:math' as math;
+import 'package:customer_app/screens/dashboard/bottom_navigation_view/quick_tracker_add_icon.dart';
+
 import '../dashboard_theme.dart';
 import './../../../model/dashboard/tabIcon_data.dart';
 import './../../../constants/colors.dart';
 import 'package:flutter/material.dart';
-
 
 class BottomBarView extends StatefulWidget {
   const BottomBarView(
@@ -78,14 +79,14 @@ class _BottomBarViewState extends State<BottomBarView>
                                     widget.changeIndex(1);
                                   }),
                             ),
-                            SizedBox(
-                              width: Tween<double>(begin: 0.0, end: 1.0)
-                                      .animate(CurvedAnimation(
-                                          parent: animationController,
-                                          curve: Curves.fastOutSlowIn))
-                                      .value *
-                                  64.0,
-                            ),
+                            // SizedBox(
+                            //   width: Tween<double>(begin: 0.0, end: 1.0)
+                            //           .animate(CurvedAnimation(
+                            //               parent: animationController,
+                            //               curve: Curves.fastOutSlowIn))
+                            //           .value *
+                            //       64.0,
+                            // ),
                             Expanded(
                               child: TabIcons(
                                   tabIconData: widget.tabIconsList[2],
@@ -117,68 +118,63 @@ class _BottomBarViewState extends State<BottomBarView>
             );
           },
         ),
-        Padding(
-          padding:
-              EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
-          child: SizedBox(
-            width: 38 * 2.0,
-            height: 38 + 62.0,
-            child: Container(
-              alignment: Alignment.topCenter,
-              color: Colors.transparent,
-              child: SizedBox(
-                width: 38 * 2.0,
-                height: 38 * 2.0,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ScaleTransition(
-                    alignment: Alignment.center,
-                    scale: Tween<double>(begin: 0.0, end: 1.0).animate(
-                        CurvedAnimation(
-                            parent: animationController,
-                            curve: Curves.fastOutSlowIn)),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: DashboardTheme.nearlyDarkBlue,
-                        gradient: LinearGradient(
-                            colors: [
-                              DashboardTheme.nearlyDarkBlue,
-                              HexColor('#6A88E5'),
-                            ],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight),
-                        shape: BoxShape.circle,
-                        boxShadow: <BoxShadow>[
-                          BoxShadow(
-                              color: DashboardTheme.nearlyDarkBlue
-                                  .withOpacity(0.4),
-                              offset: const Offset(8.0, 16.0),
-                              blurRadius: 16.0),
-                        ],
-                      ),
-                      child: Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          splashColor: Colors.white.withOpacity(0.1),
-                          highlightColor: Colors.transparent,
-                          focusColor: Colors.transparent,
-                          onTap: () {
-                            widget.addClick();
-                          },
-                          child: Icon(
-                            Icons.add,
-                            color: DashboardTheme.white,
-                            size: 32,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
+        // Padding(
+        //   padding:
+        //       EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
+        //   child: SizedBox(
+        //     width: 38 * 2.0,
+        //     height: 38 + 62.0,
+        //     child: Container(
+        //       alignment: Alignment.topCenter,
+        //       color: Colors.transparent,
+        //       child: SizedBox(
+        //         width: 38 * 2.0,
+        //         height: 38 * 2.0,
+        //         child: Padding(
+        //           padding: const EdgeInsets.all(8.0),
+        //           child: ScaleTransition(
+        //             alignment: Alignment.center,
+        //             scale: Tween<double>(begin: 0.0, end: 1.0).animate(
+        //                 CurvedAnimation(
+        //                     parent: animationController,
+        //                     curve: Curves.fastOutSlowIn)),
+        //             child: Container(
+        //                 decoration: BoxDecoration(
+        //                   color: DashboardTheme.nearlyDarkBlue,
+        //                   gradient: LinearGradient(
+        //                       colors: [
+        //                         DashboardTheme.nearlyDarkBlue,
+        //                         HexColor('#6A88E5'),
+        //                       ],
+        //                       begin: Alignment.topLeft,
+        //                       end: Alignment.bottomRight),
+        //                   shape: BoxShape.circle,
+        //                   boxShadow: <BoxShadow>[
+        //                     BoxShadow(
+        //                         color: DashboardTheme.nearlyDarkBlue
+        //                             .withOpacity(0.4),
+        //                         offset: const Offset(8.0, 16.0),
+        //                         blurRadius: 16.0),
+        //                   ],
+        //                 ),
+        //                 child: Material(
+        //                   color: Colors.transparent,
+        //                   child: InkWell(
+        //                     splashColor: Colors.white.withOpacity(0.1),
+        //                     highlightColor: Colors.transparent,
+        //                     focusColor: Colors.transparent,
+        //                     onTap: () {
+        //                       widget.addClick();
+        //                     },
+        //                     child: QuickTrackerAddIcon(),
+        //                   ),
+        //                 )),
+        //           ),
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        // ),
       ],
     );
   }
@@ -341,22 +337,22 @@ class TabClipper extends CustomClipper<Path> {
     path.lineTo(0, 0);
     path.arcTo(Rect.fromLTWH(0, 0, radius, radius), degreeToRadians(180),
         degreeToRadians(90), false);
-    path.arcTo(
-        Rect.fromLTWH(
-            ((size.width / 2) - v / 2) - radius + v * 0.04, 0, radius, radius),
-        degreeToRadians(270),
-        degreeToRadians(70),
-        false);
+    // path.arcTo(
+    //     Rect.fromLTWH(
+    //         ((size.width / 2) - v / 2) - radius + v * 0.04, 0, radius, radius),
+    //     degreeToRadians(270),
+    //     degreeToRadians(70),
+    //     false);
 
-    path.arcTo(Rect.fromLTWH((size.width / 2) - v / 2, -v / 2, v, v),
-        degreeToRadians(160), degreeToRadians(-140), false);
+    // path.arcTo(Rect.fromLTWH((size.width / 2) - v / 2, -v / 2, v, v),
+    //     degreeToRadians(160), degreeToRadians(-140), false);
 
-    path.arcTo(
-        Rect.fromLTWH((size.width - ((size.width / 2) - v / 2)) - v * 0.04, 0,
-            radius, radius),
-        degreeToRadians(200),
-        degreeToRadians(70),
-        false);
+    // path.arcTo(
+    //     Rect.fromLTWH((size.width - ((size.width / 2) - v / 2)) - v * 0.04, 0,
+    //         radius, radius),
+    //     degreeToRadians(200),
+    //     degreeToRadians(70),
+    //     false);
     path.arcTo(Rect.fromLTWH(size.width - radius, 0, radius, radius),
         degreeToRadians(270), degreeToRadians(90), false);
     path.lineTo(size.width, 0);
---
+++ lib/screens/dashboard/bottom_navigation_view/quick_tracker_add_icon.dart
@@ -0,0 +1,237 @@
+import 'package:customer_app/screens/dashboard/home/trackers/bmi_tracker/bmi_tracker_home.dart';
+import 'package:customer_app/screens/dashboard/home/trackers/calorie_tracker/calorie_tracker_home.dart';
+import 'package:customer_app/screens/dashboard/home/trackers/water_tracker/water_tracker_home.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../dashboard_theme.dart';
+
+class QuickTrackerAddIcon extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => QuickTrackerAddIconState();
+}
+
+class QuickTrackerAddIconState extends State<QuickTrackerAddIcon>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation degOneTranslationAnimation,
+      degTwoTranslationAnimation,
+      degThreeTranslationAnimation;
+  Animation rotationAnimation;
+
+  Animation mainButtonrotationAnimation;
+
+  @override
+  void initState() {
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 250));
+    degOneTranslationAnimation = TweenSequence([
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 1.2), weight: 75.0),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.2, end: 1.0), weight: 25.0),
+    ]).animate(animationController);
+    degTwoTranslationAnimation = TweenSequence([
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 1.4), weight: 55.0),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.4, end: 1.0), weight: 45.0),
+    ]).animate(animationController);
+    degThreeTranslationAnimation = TweenSequence([
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 1.75), weight: 35.0),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.75, end: 1.0), weight: 65.0),
+    ]).animate(animationController);
+    rotationAnimation = Tween<double>(begin: 180.0, end: 0.0).animate(
+        CurvedAnimation(parent: animationController, curve: Curves.easeOut));
+    mainButtonrotationAnimation = Tween<double>(begin: 180.0, end: 45.0)
+        .animate(CurvedAnimation(
+            parent: animationController, curve: Curves.easeOut));
+    super.initState();
+    animationController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  double getRadiansFromDegree(double degree) {
+    double unitRadian = 57.295779513;
+    return degree / unitRadian;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Material(
+        color: Colors.transparent,
+        child: Center(
+            child: Container(
+                height: 60,
+                child: Stack(
+                  children: <Widget>[
+                    // GestureDetector(
+                    //   onTap: () {
+                    //     print("onvl");
+                    //   },
+                    //   child: InkWell(
+                    //     child: Transform.translate(
+                    //       offset: Offset.fromDirection(
+                    //           getRadiansFromDegree(330),
+                    //           degOneTranslationAnimation.value * 100),
+                    //       child: Transform(
+                    //         transform: Matrix4.rotationZ(
+                    //             getRadiansFromDegree(rotationAnimation.value))
+                    //           ..scale(degOneTranslationAnimation.value),
+                    //         alignment: Alignment.center,
+                    //         child: CircularButton(
+                    //           color: Colors.transparent,
+                    //           width: 100,
+                    //           height: 100,
+                    //           icon: Image.asset(
+                    //             'assets/images/dashboard/glass.png',
+                    //           ),
+                    //           onClick: () {
+                    //             Navigator.of(context).push(MaterialPageRoute(
+                    //                 builder: (BuildContext context) =>
+                    //                     WaterTrackerHomeScreen()));
+                    //           },
+                    //         ),
+                    //       ),
+                    //     ),
+                    //   ),
+                    // ),
+                    // Transform.translate(
+                    //   offset: Offset.fromDirection(getRadiansFromDegree(275),
+                    //       degTwoTranslationAnimation.value * 100),
+                    //   child: Transform(
+                    //     transform: Matrix4.rotationZ(
+                    //         getRadiansFromDegree(rotationAnimation.value))
+                    //       ..scale(degTwoTranslationAnimation.value),
+                    //     alignment: Alignment.center,
+                    //     child: CircularButton(
+                    //       color: Colors.transparent,
+                    //       width: 100,
+                    //       height: 100,
+                    //       icon: SvgPicture.asset(
+                    //         'assets/svgs/calorie.svg',
+                    //         width: 50,
+                    //       ),
+                    //       onClick: () {
+                    //         Navigator.of(context).push(MaterialPageRoute(
+                    //             builder: (BuildContext context) =>
+                    //                 CalorietrackerHomeScreen()));
+                    //       },
+                    //     ),
+                    //   ),
+                    // ),
+                    SizedBox(
+                      child:
+                    Transform.translate(
+                      offset: Offset.fromDirection(getRadiansFromDegree(220),
+                          degThreeTranslationAnimation.value * 100),
+                      child: Transform(
+                        transform: Matrix4.rotationZ(
+                            getRadiansFromDegree(rotationAnimation.value))
+                          ..scale(degThreeTranslationAnimation.value),
+                        alignment: Alignment.center,
+                        child: CircularButton(
+                          color: Colors.transparent, //Color(0XFF265BC5),
+                          width: 100,
+                          height: 100,
+                          icon: Icon(Icons.ac_unit)
+                          
+                          // SvgPicture.asset(
+                          //   'assets/svgs/weight.svg',
+                          //   width: 50,
+                          // )
+                          ,
+                          onClick: () {
+                            print("press");
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    BmitrackerHomeScreen()));
+                          },
+                        ),
+                      ),
+                    ),
+                    ),
+
+                    
+                    Transform(
+                      transform: Matrix4.rotationZ(getRadiansFromDegree(
+                          mainButtonrotationAnimation.value)),
+                      alignment: Alignment.center,
+                      child: CircularCenterButton(
+                        color: Colors.indigo,
+                        width: 60,
+                        height: 60,
+                        icon: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                        onClick: () {
+                          print("onclick center");
+                          if (animationController.isCompleted) {
+                            animationController.reverse();
+                          } else {
+                            animationController.forward();
+                          }
+                        },
+                      ),
+                    )
+                  ],
+                ))));
+  }
+}
+
+class CircularCenterButton extends StatelessWidget {
+  final double width;
+  final double height;
+  final Color color;
+  final Widget icon;
+  final Function onClick;
+
+  CircularCenterButton(
+      {this.color, this.width, this.height, this.icon, this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        shape: BoxShape.circle,
+      ),
+      width: width,
+      height: height,
+      child: IconButton(icon: icon, enableFeedback: true, onPressed: onClick),
+    );
+  }
+}
+
+class CircularButton extends StatelessWidget {
+  final double width;
+  final double height;
+  final Color color;
+  final Widget icon;
+  final Function onClick;
+
+  CircularButton(
+      {this.color, this.width, this.height, this.icon, this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration:
+          BoxDecoration(color: color, shape: BoxShape.circle, boxShadow: [
+        BoxShadow(
+            color: DashboardTheme.nearlyDarkBlue.withOpacity(0.4),
+            offset: const Offset(8.0, 12.0),
+            blurRadius: 12.0),
+      ]),
+      width: width,
+      height: height,
+      child: IconButton(icon: icon, enableFeedback: true, onPressed: onClick),
+    );
+  }
+}
